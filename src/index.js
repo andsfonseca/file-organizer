0401ea6bd4219dff4e64201a02516e2f55759784
andsfonseca <andsf@live.com>
2021-07-16 15:30:58 -0300

    feat: Preparação para usar 'npm link' e arquivo de teste

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-export function cli(args) {
-    console.log(args);
-}
+#!/usr/bin/env node
+
+/**
+ * Tarefas a ser implementadas
+ * 
+ * 1. Ler os arquivos da pasta onde é executado.
+ * 2. Ler os argumentos da linha de comando
+ * 3. Mover e Renomear Arquivos
+ * 4. Criar pastas
+ * 5. Ler um JSON
+ * 6. Criar um Template de Organização de Arquivos (Imagens, Videos, Executáveis, etc)
+ * 7. Lógica Final
+ */
+
+console.log("Hello World")
