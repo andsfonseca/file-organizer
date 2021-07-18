c8abd23f604330dcd053c9f3a085aede3ecae0de
andsfonseca <andsf@live.com>
2021-07-17 23:46:33 -0300

    feat: Leitura de Arquivo, Movimentação de Arquivos e Template padrão

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const commander = require('commander')
+const path = require('path')
 
 /**
  * Tarefas a ser implementadas
  * 
- * 1. Ler os arquivos da pasta onde é executado.
- * 2. Ler os argumentos da linha de comando
+ * 1. Ler os arquivos da pasta onde é executado. OK
+ * 2. Ler os argumentos da linha de comando OK
  * 3. Mover e Renomear Arquivos
- * 4. Criar pastas
+ * 4. Criar pastas OK
  * 5. Ler um JSON
  * 6. Criar um Template de Organização de Arquivos (Imagens, Videos, Executáveis, etc)
- * 7. Lógica Final
+ * 7. Recuperar a extensão de um arquivo
+ * 8. Lógica Final
+ */
+
+/**
+ * Lê os arquivos de uma pasta
+ * @param {String} path Caminho da pasta
+ * @returns Retorna uma lista em String dos arquivos de uma pasta
  */
 const readFolder = (path) => {
     let files = fs.readdirSync(path)
@@ -19,25 +27,66 @@ const readFolder = (path) => {
     return onlyFiles
 }
 
+/**
+ * Cria uma pasta com um nome especificado no caminho informado.
+ * @param {String} path Caminho da pasta
+ * @param {String} folderName Nome da pasta
+ */
 const createFolder = (path, folderName) => {
     let temp = path + "/" + folderName
-    if (!fs.existsSync(temp)){
+    if (!fs.existsSync(temp)) {
         fs.mkdirSync(temp)
     }
 }
 
-const main = (path) => {
-    console.log(createFolder(path, "foi"))
-    
+/**
+ * Mover arquivos de um caminho para outro caminho
+ * @param {String} path Caminho Original
+ * @param {String} newPath Caminho de destino
+ * @param {String} filename Nome do Arquivo
+ * @returns Verdadeiro se conseguiu mover, falso se não conseguiu
+ */
+const moveFile = (path, newPath, filename) => {
+    try {
+        fs.renameSync(path + "/" + filename, newPath + "/" + filename)
+        return true;
+    }
+    catch (e) {
+        return false;
+    }
 }
 
-commander
-.version('1.0.0')
-.name('file-organizer')
-.description('A simple file organizer by extension and groups.')
-.option('-p, --path [folder]', 'Directory of the path to be organized', '.')
-.action((args) => {
-    main(args.path)
-})
-.parse(process.argv)
+/**
+ * Lê um JSON e retorna como um dicionário
+ * @param {String} filepath Caminho do Json
+ * @returns Um dicionário
+ */
+const readJSON = (filepath) => {
+    let rawdata = fs.readFileSync(filepath);
+    let json = JSON.parse(rawdata);
+    return json
+}
+
+
+const organizeFolder = (path) => {
+    console.log(readJSON("default.json").Images)
+    // moveFile(path, path + "/" + "teste", "dasd")
+}
+
+
+const cli = () => {
+    commander.version('1.0.0')
+        .name('file-organizer')
+        .description('A simple file organizer by extension and groups.')
+        .option('-p, --path [folder]', 'Directory of the path to be organized', '.')
+        .action((args) => {
+            let path = args.path
+            organizeFolder(path)
+        })
+        .parse(process.argv)
+}
+
+cli();
+
+
 
